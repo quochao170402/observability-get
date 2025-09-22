a155741353c9e266624fa6fea1a96461d29362a1
quochao <hao.bui@herond.org>
2025-09-22 13:44:50 +0700

    init

---
+++ Register.cs
@@ -0,0 +1,15 @@
+namespace TestMonitoring;
+
+public class Register
+{
+    public string Name { get; set; }
+}
+
+public static class Add
+{
+    public static void Adddd(string name)
+    {
+        var register = new Register();
+        register.Name = name;
+    }
+}
